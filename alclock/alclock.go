8acb14ca376dc828ccc557794411fa01a9777378
Sam Scheele <sam.scheele@outlook.com>
2016-11-08 17:04:28 -0500

    Cursor position working

--- alclock/alclock.go
+++ alclock/alclock.go
@@ -1,27 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"os"
 	"os/exec"
+	"strings"
 
 	"fmt"
 	"time"
 )
 
 const numDataRows = 4
-const banner = `
- |_| _ | | _
+const banner = ` |_| _ | | _
  | |(_|| |(_)
 `
 
+type Alarm struct {
+	//DateTime should have the following indices: "year", "month", "day", "hour", "minute", "second"
+	//An asterisk ("*") matches everything
+	DateTime map[string]string
+	//DayOfWeek contains a key for each day of the week
+	DayOfWeek map[time.Weekday]struct{}
+}
+
+var Alarms []Alarm
+var ch chan string
+
 func main() {
-	initScreen()
-	ch := make(chan string)
-	go runClock(ch)
-	for i := 0; ; i++ {
-		ch <- fmt.Sprint(i)
-	}
+	//initScreen()
+
+	ch = make(chan string)
+	/*
+		for {
+			x, _ := cursorPos()
+			fmt.Print(x)
+			time.Sleep(2 * time.Second)
+		}
+	*/
+	runClock(ch)
+	/*
+		//put test numbers into the data field to show it's working
+		for i := 0; ; i++ {
+			ch <- fmt.Sprint(i)
+		}
+	*/
 }
 
 func runClock(ch chan string) {
@@ -54,6 +77,9 @@ func initScreen() {
 
 func updateScreen(timeField string, dataFields []string) {
 	width, height, err := termSize()
+
+	x, _ := cursorPos(height, width) //cursorPos(height, width)
+
 	if err != nil {
 		fmt.Println("Error getting terminal size")
 	}
@@ -61,9 +87,10 @@ func updateScreen(timeField string, dataFields []string) {
 	fmt.Print(timeField)
 
 	for i, s := range dataFields {
-		fmt.Printf("\033[%d;0H", (height-numDataRows)+i+1)
-		fmt.Printf(fmt.Sprintf("%% -%ds", width-1), s)
+		fmt.Printf("\033[%d;0H", (height-numDataRows)+i)
+		fmt.Print(s)
 	}
+	fmt.Printf("\033[%d;%dH", height, x)
 }
 
 func termSize() (x int, y int, err error) {
@@ -75,4 +102,62 @@ func termSize() (x int, y int, err error) {
 	}
 	fmt.Fscanf(bytes.NewReader(out), "%d %d", &y, &x)
 	return
+}
+
+func cursorPos(height, width int) (x int, err error) {
+	if err != nil {
+		return
+	}
+	enableRaw := exec.Command("stty", "raw")
+	enableRaw.Stdin = os.Stdin
+	enableRaw.Run()
+
+	cmd := exec.Command("echo", "-e", fmt.Sprintf("%c[6n", 27))
+	randomBytes := &bytes.Buffer{}
+	cmd.Stdout = randomBytes
+
+	// Start command asynchronously
+	_ = cmd.Start()
+
+	// capture keyboard output from echo command
+	reader := bufio.NewReader(os.Stdin)
+	cmd.Wait()
+
+	// by printing the command output, we are triggering input
+	fmt.Print(randomBytes)
+	text, _ := reader.ReadString('R') // how to get this to not require manual newline?
+	text = trimPreJunk(text)
+
+	disableRaw := exec.Command("stty", "-raw")
+	disableRaw.Stdin = os.Stdin
+	disableRaw.Run()
+
+	for i := 2; i < height; i++ {
+		fmt.Printf("\033[%d;0H", i)
+		fmt.Printf(fmt.Sprintf("%% %ds", width-1), "")
+	}
+	fmt.Printf("\033[%d;0H", 0)
+	fmt.Print(banner)
+
+	fmt.Fscanf(strings.NewReader(text), "%dR", &x)
+	fmt.Printf("\033[%d;%dH", height, x)
+	fmt.Printf(fmt.Sprintf("%% %ds", width-x), "")
+	go func() {
+		ch <- fmt.Sprintf("Cursor pos is (%d, %d)", x, height)
+	}()
+
+	return
+}
+
+func trimPreJunk(s string) string {
+	for i := 0; i < len(s); i++ {
+		if s[i] == ';' {
+			return s[i+1:]
+		}
+	}
+	return ""
+}
+
+func addAlarm(s string) {
+
 }
