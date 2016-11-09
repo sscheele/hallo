d30502969f7c5acd2083d805b9e71b65e58590c9
Sam Scheele <sam.scheele@outlook.com>
2016-11-09 15:01:42 -0500

    Secure auth for api

--- gmaps/helper.go
+++ gmaps/helper.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"golang.org/x/net/context"
 	"googlemaps.github.io/maps"
 )
 
 func main() {
-	c, err := maps.NewClient(maps.WithAPIKey(" AIzaSyCO8qw67aswZVM-3-GPIxPEBJwDIArxjsY"))
+	f, err := os.Open("api-key.txt")
+	if err != nil {
+		fmt.Println("Error reading in API key!")
+		return
+	}
+	reader := bufio.NewReader(f)
+	text, _ := reader.ReadString('\n')
+	c, err := maps.NewClient(maps.WithAPIKey(text))
 	if err != nil {
 		fmt.Println("fatal error: %s", err)
 		return
