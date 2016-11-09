943418ffb71c33d03cb2954fa3b761ed4265cf49
Sam Scheele <sam.scheele@outlook.com>
2016-11-09 16:40:24 -0500

    add almost working gmaps code

--- gmaps/main.go
+++ gmaps/main.go
@@ -1,19 +1,3 @@
-// Copyright 2015 Google Inc. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Package main contains a simple command line tool for Directions API
-// Directions docs: https://developers.google.com/maps/documentation/directions/
 package main
 
 import (
@@ -57,7 +41,7 @@ func main() {
 		Destination:  destination,
 		ArrivalTime:  arrivalTime,
 		Alternatives: alternatives,
-		Mode:         maps.TravelModeWalking,
+		Mode:         maps.TravelModeDriving,
 	}
 
 	lookupTrafficModel(trafficModel, r)
@@ -71,11 +55,6 @@ func main() {
 
 	fmt.Println(waypoints)
 	fmt.Println(routes)
-}
-
-type iterationResult struct {
-	result string
-	err    error
 }
 
 func lookupTrafficModel(trafficModel string, r *maps.DirectionsRequest) {
