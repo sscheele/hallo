f55ae46011954153486e77e18efe3155bd17ca34
Sam Scheele <sam.scheele@outlook.com>
2016-11-09 18:18:38 -0500

    Start UI work

--- alclock/alclock.go
+++ alclock/alclock.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"os"
-	"os/exec"
-	"strings"
-
 	"fmt"
+	"log"
 	"time"
+
+	"github.com/jroimartin/gocui"
 )
 
 const numDataRows = 4
 
-var banner = []string{" |_| _ | | _ ", " | |(_|| |(_)"}
+const banner = ` |_| _ | | _
+ | |(_|| |(_)`
 
 type Alarm struct {
 	//DateTime should have the following indices: "year", "month", "day", "hour", "minute", "second"
@@ -24,141 +22,83 @@ type Alarm struct {
 }
 
 var Alarms []Alarm
-var ch chan string
+
+func setCurrentViewOnTop(g *gocui.Gui, name string) (*gocui.View, error) {
+	if _, err := g.SetCurrentView(name); err != nil {
+		return nil, err
+	}
+	return g.SetViewOnTop(name)
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+
+	if v, err := g.SetView("title", 0, 0, maxX-1, 3); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Frame = false
+		v.Wrap = true
+		fmt.Fprint(v, banner)
+	}
+	if v, err := g.SetView("time", maxX/2-5, maxY/2-2, maxX-1, maxY/2+1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Frame = false
+	}
+	if v, err := g.SetView("data", 0, maxY-8, maxX-1, maxY-3); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Frame = false
+		v.Wrap = true
+		v.Autoscroll = true
+	}
+
+	if v, err := g.SetView("input", 0, maxY-3, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Editable = true
+		v.Frame = false
+
+		if _, err = setCurrentViewOnTop(g, "input"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
 
 func main() {
-	//initScreen()
-
-	ch = make(chan string)
-	runClock(ch)
-}
-
-func runClock(ch chan string) {
-	dataStrs := make([]string, numDataRows)
-	for {
-		t := time.Now()
-		select {
-		case x, ok := <-ch:
-			if ok {
-				dataStrs = append(dataStrs[1:], x)
-			} else {
-				return
-			}
-		default:
-
-		}
-
-		updateScreen(fmt.Sprintf("%02d:%02d:%02d", t.Hour(), t.Minute(), t.Second()), dataStrs)
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func moveCursorTo(x, y int) {
-	fmt.Printf("\033[%d;%dH", y, x)
-}
-
-func initScreen() {
-	cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-
-	/*
-		moveCursorTo(1, 1)
-		fmt.Print(banner[0])
-		moveCursorTo(1, 2)
-		fmt.Print(banner[1])
-	*/
-}
-
-func updateScreen(timeField string, dataFields []string) {
-	width, height, err := termSize()
-
-	x, _ := cursorPos(height, width) //cursorPos(height, width)
-
-	if err != nil {
-		fmt.Println("Error getting terminal size")
-	}
-	moveCursorTo(width/2, height/2)
-	fmt.Print(timeField)
-
-	for i, s := range dataFields {
-		moveCursorTo(0, (height-numDataRows)+i)
-		fmt.Print(s)
-	}
-
-	moveCursorTo(x, height)
-}
-
-func termSize() (x int, y int, err error) {
-	cmd := exec.Command("stty", "size")
-	cmd.Stdin = os.Stdin
-	out, err := cmd.Output()
+	g, err := gocui.NewGui()
 	if err != nil {
 		return
 	}
-	fmt.Fscanf(bytes.NewReader(out), "%d %d", &y, &x)
-	return
-}
+	defer g.Close()
 
-func cursorPos(height, width int) (x int, err error) {
-	if err != nil {
+	g.Highlight = true
+	g.Cursor = true
+	g.SelFgColor = gocui.ColorGreen
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		return
 	}
-	enableRaw := exec.Command("stty", "raw")
-	enableRaw.Stdin = os.Stdin
-	enableRaw.Run()
 
-	cmd := exec.Command("echo", "-e", fmt.Sprintf("%c[6n", 27))
-	randomBytes := &bytes.Buffer{}
-	cmd.Stdout = randomBytes
-
-	// Start command asynchronously
-	_ = cmd.Start()
-
-	// capture keyboard output from echo command
-	reader := bufio.NewReader(os.Stdin)
-	cmd.Wait()
-
-	// by printing the command output, we are triggering input
-	fmt.Print(randomBytes)
-	text, _ := reader.ReadString('R')
-	text = trimPreJunk(text)
-
-	disableRaw := exec.Command("stty", "-raw")
-	disableRaw.Stdin = os.Stdin
-	disableRaw.Run()
-
-	for i := 3; i < height; i++ {
-		moveCursorTo(0, i)
-		fmt.Printf(fmt.Sprintf("%% -%ds", width), "")
-	}
 	/*
-		moveCursorTo(1, 1)
-		fmt.Print(banner[0])
-		moveCursorTo(1, 2)
-		fmt.Print(banner[1])
+		go func() {
+			t := time.Now()
+			fmt.Fprintf(v, "%02d:%02d:%02d", t.Hour(), t.Minute(), t.Second())
+		}()
 	*/
 
-	fmt.Fscanf(strings.NewReader(text), "%dR", &x)
-	moveCursorTo(x, height)
-	fmt.Printf(fmt.Sprintf("%% -%ds", width-x), "")
-
-	go func() {
-		ch <- fmt.Sprintf("Cursor pos is (%d, %d)", x, height)
-	}()
-
-	return
-}
-
-func trimPreJunk(s string) string {
-	for i := 0; i < len(s); i++ {
-		if s[i] == ';' {
-			return s[i+1:]
-		}
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
 	}
-	return ""
-}
-
-func addAlarm(s string) {
-
 }
